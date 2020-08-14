26a9c303ea795eba1a166578acd9ed6f77920581
lqsullivan <logan.q.sullivan@gmail.com>
2020-08-13 21:39:51 -0500

    skip class 2 answers cause they don't have their own question page

--- R/extract_matchday_data.R
+++ R/extract_matchday_data.R
@@ -15,7 +15,10 @@ extract_matchday_data <- function(page){
     stringr::str_replace("[\\n\\tQ\\d\\.]+", "") %>%
     stringr::str_replace("([A-Z/\\s])-.*", "\\1") %>%
     stringr::str_replace("[\\n\\t]+", "") %>%
-    stringr::str_replace("\\s$", "")
+    stringr::str_replace("\\s$", "") %>%
+    # league 57 has class 1 and 2, we're skipping class 2 cause there's no
+    # question page
+    `[`(1:6)
 
   return(tibble(category))
 }
